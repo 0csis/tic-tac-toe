83e0d4f15c45e9e4055058989ffe86cb2d85aca5
0csis <121513547+0csis@users.noreply.github.com>
2023-04-24 18:41:07 -0300

    finish js file

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,32 @@ const gameBoard = (() => {
 
 const Player = (weapon) => game.playGame(weapon);
 
+function playAgain() {
+  const game = document.querySelector('.game');
+  game.remove();
+  const again = document.querySelector('.again');
+  again.textContent = 'Play again?';
+  const yes = document.querySelector('.yes');
+  yes.textContent = 'Yes';
+  const no = document.querySelector('.no');
+  no.textContent = 'No';
+
+  const buttons = document.querySelectorAll('button');
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      if (btn.id == 'yes') {
+        location.reload();
+      }
+    });
+  });
+}
+
+function endGame(winner) {
+  const winnerGame = document.querySelector('.winner');
+  winnerGame.textContent = `${winner} wins!`;
+  playAgain();
+}
+
 function checkGame(table) {
   if (table[0] == 'X' && table[1] == 'X' && table[2] == 'X'
       || table[3] == 'X' && table[4] == 'X' && table[5] == 'X'
@@ -14,7 +40,7 @@ function checkGame(table) {
       || table[0] == 'X' && table[4] == 'X' && table[8] == 'X'
       || table[2] == 'X' && table[5] == 'X' && table[8] == 'X'
       || table[2] == 'X' && table[4] == 'X' && table[6] == 'X') {
-    console.log('a');
+    endGame('X');
   } else if (table[0] == 'O' && table[1] == 'O' && table[2] == 'O'
       || table[3] == 'O' && table[4] == 'O' && table[5] == 'O'
       || table[0] == 'O' && table[3] == 'O' && table[6] == 'O'
@@ -23,23 +49,50 @@ function checkGame(table) {
       || table[0] == 'O' && table[4] == 'O' && table[8] == 'O'
       || table[2] == 'O' && table[5] == 'O' && table[8] == 'O'
       || table[2] == 'O' && table[4] == 'O' && table[6] == 'O') {
-    console.log('a');
+    endGame('O');
   }
 }
 
 function playComputer(player) {
   if (player == 'X') {
-    console.log('b');
+    const computer = Math.floor((Math.random() * 8) + 1);
+    if (gameBoard.gameboard[computer] === '') {
+      gameBoard.gameboard[computer] = 'O';
+      checkGame(gameBoard.gameboard);
+      console.log(computer);
+      const addComputer = document.getElementById(`${computer}`);
+      addComputer.textContent = 'O';
+    } else {
+      playComputer(player);
+    }
+  } else {
+    const computer = Math.floor((Math.random() * 8) + 1);
+    if (gameBoard.gameboard[computer] === '') {
+      gameBoard.gameboard[computer] = 'X';
+      checkGame(gameBoard.gameboard);
+      console.log(computer);
+      const addComputer = document.getElementById(`${computer}`);
+      addComputer.textContent = 'X';
+    } else {
+      playComputer(player);
+    }
   }
 }
 
 const game = (() => {
+  const choose = document.querySelector('.choose');
   const chosen = document.querySelectorAll('.chosen');
-  const Weapon = chosen.forEach((o_x) => {
+  let Weapon = chosen.forEach((o_x) => {
     o_x.addEventListener('click', () => {
       if (o_x.id == 'x') {
+        Weapon = 'X';
+        document.getElementById('o').remove();
+        choose.textContent = 'Choose X or O';
         Player('X');
       } else {
+        Weapon = 'O';
+        document.getElementById('x').remove();
+        choose.textContent = 'Choose X or O';
         Player('O');
       }
     });
@@ -56,7 +109,7 @@ const game = (() => {
           gameBoard.gameboard[square.id] = player;
           square.textContent = player;
           checkGame(gameBoard.gameboard);
-          playComputer(Weapon);
+          playComputer(player);
         }
       });
     });
